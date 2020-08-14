4f67c1f74ea72c00e6cad48d27bc7a053724328f
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-08-14 12:02:06 +0300

    Deleted some code

--- jaxfenics_adjoint/core.py
+++ jaxfenics_adjoint/core.py
@@ -13,56 +13,12 @@ from jax.api import defjvp_all
 import functools
 import itertools
 
-from fenics_numpy import (
-    # numpy_to_fenics,
-    # fenics_to_numpy,
-    evaluate_primal,
-    evaluate_vjp,
-    evaluate_jvp,
-)
+from fenics_numpy import evaluate_primal, evaluate_vjp, evaluate_jvp
 
-# from .helpers import (
-#     numpy_to_fenics,
-#     fenics_to_numpy,
-#     get_numpy_input_templates,
-#     check_input,
-#     convert_all_to_fenics,
-# )
 from .helpers import FenicsVariable
 from .helpers import jax_to_fenics_numpy
 
 from typing import Type, List, Union, Iterable, Callable, Tuple
-
-
-# def fem_eval(
-#     fenics_function: Callable,
-#     fenics_templates: Iterable[FenicsVariable],
-#     *args: np.array,
-# ) -> Tuple[np.array, FenicsVariable, Tuple[FenicsVariable], pyadjoint.Tape]:
-#     """Computes the output of a fenics_function and saves a corresponding gradient tape
-#     Input:
-#         fenics_function (callable): FEniCS function to be executed during the forward pass
-#         fenics_templates (iterable of FenicsVariable): Templates for converting arrays to FEniCS types
-#         args (tuple): jax array representation of the input to fenics_function
-#     Output:
-#         numpy_output (np.array): JAX array representation of the output from fenics_function(*fenics_inputs)
-#         residual_form (ufl.Form): UFL Form for the residual used to solve the problem with fenics.solve(F==0, ...)
-#         fenics_inputs (list of FenicsVariable): FEniCS representation of the input args
-#     """
-
-#     check_input(fenics_templates, *args)
-#     fenics_inputs = convert_all_to_fenics(fenics_templates, *args)
-
-#     # Create tape associated with this forward pass
-#     tape = pyadjoint.Tape()
-#     pyadjoint.set_working_tape(tape)
-#     fenics_output = fenics_function(*fenics_inputs)
-
-#     if isinstance(fenics_output, tuple):
-#         raise ValueError("Only single output from FEniCS function is supported.")
-
-#     numpy_output = np.asarray(fenics_to_numpy(fenics_output))
-#     return numpy_output, fenics_output, fenics_inputs, tape
 
 
 def vjp_fem_eval(
@@ -139,65 +95,9 @@ def vjp_fem_eval(
     return numpy_output, vjp_fun1
 
 
-# @trace("vjp_fem_eval_impl")
-# def vjp_fem_eval_impl(
-#     g: np.array,
-#     fenics_output: FenicsVariable,
-#     fenics_inputs: Iterable[FenicsVariable],
-#     tape: pyadjoint.Tape,
-# ) -> Tuple[np.array]:
-#     """Computes the gradients of the output with respect to the inputs."""
-#     # Convert tangent covector (adjoint) to a FEniCS variable
-#     adj_value = numpy_to_fenics(g, fenics_output)
-#     if isinstance(adj_value, (fenics.Function, fenics_adjoint.Function)):
-#         adj_value = adj_value.vector()
-
-#     tape.reset_variables()
-#     fenics_output.block_variable.adj_value = adj_value
-#     with tape.marked_nodes(fenics_inputs):
-#         tape.evaluate_adj(markings=True)
-#     fenics_grads = [fi.block_variable.adj_value for fi in fenics_inputs]
-
-#     # Convert FEniCS gradients to jax array representation
-#     jax_grads = (
-#         None if fg is None else np.asarray(fenics_to_numpy(fg)) for fg in fenics_grads
-#     )
-
-#     jax_grad_tuple = tuple(jax_grads)
-
-#     return jax_grad_tuple
-
-
-# def jvp_fem_eval(
-#     fenics_function: Callable,
-#     fenics_templates: Iterable[FenicsVariable],
-#     primals: Tuple[np.array],
-#     tangents: Tuple[np.array],
-# ) -> Tuple[np.array]:
-#     """Computes the tangent linear model
-#     """
-
-#     numpy_output_primal, fenics_output_primal, fenics_primals, tape = fem_eval(
-#         fenics_function, fenics_templates, *primals
-#     )
-
-#     # Now tangent evaluation!
-#     tape.reset_variables()
-
-#     fenics_tangents = convert_all_to_fenics(fenics_primals, *tangents)
-#     for fp, ft in zip(fenics_primals, fenics_tangents):
-#         fp.block_variable.tlm_value = ft
-
-#     tape.evaluate_tlm()
-
-#     fenics_output_tangent = fenics_output_primal.block_variable.tlm_value
-#     jax_output_tangent = np.asarray(fenics_to_numpy(fenics_output_tangent))
-
-#     return numpy_output_primal, jax_output_tangent
-
-
 def build_jax_fem_eval(fenics_templates: FenicsVariable) -> Callable:
     """Return `f(*args) = build_jax_fem_eval(*args)(ofunc(*args))`.
+    This is for reverse mode AD.
     Given the FEniCS-side function ofunc(*args), return the function
     `f(*args) = build_jax_fem_eval(*args)(ofunc(*args))` with
     the VJP of `f`, where:
@@ -256,7 +156,8 @@ def build_jax_fem_eval(fenics_templates: FenicsVariable) -> Callable:
 # they override each other
 # therefore here I create a separate wrapped function
 def build_jax_fem_eval_fwd(fenics_templates: FenicsVariable) -> Callable:
-    """Return `f(*args) = build_jax_fem_eval(*args)(ofunc(*args))`. This is forward mode AD.
+    """Return `f(*args) = build_jax_fem_eval(*args)(ofunc(*args))`.
+    This is for forward mode AD.
     Given the FEniCS-side function ofunc(*args), return the function
     `f(*args) = build_jax_fem_eval(*args)(ofunc(*args))` with
     the JVP of `f`, where:
@@ -264,7 +165,7 @@ def build_jax_fem_eval_fwd(fenics_templates: FenicsVariable) -> Callable:
     Args:
     ofunc: The FEniCS-side function to be wrapped.
     Returns:
-    `f(args) = build_jax_fem_eval(*args)(ofunc(*args))`
+    `f(args) = build_jax_fem_eval_fwd(*args)(ofunc(*args))`
     """
 
     def decorator(fenics_function: Callable) -> Callable:
--- tests/test_assemble.py
+++ tests/test_assemble.py
@@ -10,10 +10,7 @@ import ufl
 
 import fdm
 
-# from jaxfenics_adjoint import fem_eval, vjp_fem_eval, jvp_fem_eval
 from jaxfenics_adjoint import build_jax_fem_eval
-
-# from jaxfenics_adjoint import fenics_to_numpy, numpy_to_fenics
 
 config.update("jax_enable_x64", True)
 
@@ -35,49 +32,6 @@ def assemble_fenics(u, kappa0, kappa1):
 
 templates = (fa.Function(V), fa.Constant(0.0), fa.Constant(0.0))
 inputs = (np.ones(V.dim()), np.ones(1) * 0.5, np.ones(1) * 0.6)
-# ff = lambda *args: fem_eval(assemble_fenics, templates, *args)[0]  # noqa: E731
-# ff0 = lambda x: ff(x, inputs[1], inputs[2])  # noqa: E731
-# ff1 = lambda y: ff(inputs[0], y, inputs[2])  # noqa: E731
-# ff2 = lambda z: ff(inputs[0], inputs[1], z)  # noqa: E731
-
-
-# def test_fenics_forward():
-#     numpy_output, _, _, _, = fem_eval(assemble_fenics, templates, *inputs)
-#     u1 = fa.interpolate(fa.Constant(1.0), V)
-#     J = assemble_fenics(u1, fa.Constant(0.5), fa.Constant(0.6))
-#     assert np.isclose(numpy_output, J)
-
-
-# def test_vjp_assemble_eval():
-#     numpy_output, vjp_fun = vjp_fem_eval(assemble_fenics, templates, *inputs)
-#     g = np.ones_like(numpy_output)
-#     vjp_out = vjp_fun(g)
-
-#     fdm_jac0 = fdm.jacobian(ff0)(inputs[0])
-#     fdm_jac1 = fdm.jacobian(ff1)(inputs[1])
-#     fdm_jac2 = fdm.jacobian(ff2)(inputs[2])
-
-#     check1 = np.allclose(vjp_out[0], fdm_jac0)
-#     check2 = np.allclose(vjp_out[1], fdm_jac1)
-#     check3 = np.allclose(vjp_out[2], fdm_jac2)
-#     assert check1 and check2 and check3
-
-
-# def test_jvp_assemble_eval():
-#     primals = inputs
-#     tangent0 = np.asarray(onp.random.normal(size=(V.dim(),)))
-#     tangent1 = np.asarray(onp.random.normal(size=(1,)))
-#     tangent2 = np.asarray(onp.random.normal(size=(1,)))
-#     tangents = (tangent0, tangent1, tangent2)
-
-#     fdm_jvp0 = fdm.jvp(ff0, tangents[0])(primals[0])
-#     fdm_jvp1 = fdm.jvp(ff1, tangents[1])(primals[1])
-#     fdm_jvp2 = fdm.jvp(ff2, tangents[2])(primals[2])
-
-#     _, out_tangent = jvp_fem_eval(assemble_fenics, templates, primals, tangents)
-
-#     assert np.allclose(fdm_jvp0 + fdm_jvp1 + fdm_jvp2, out_tangent)
-
 
 hh = build_jax_fem_eval(templates)(assemble_fenics)
 hh0 = lambda x: hh(x, inputs[1], inputs[2])  # noqa: E731
