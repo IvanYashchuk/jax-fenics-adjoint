655a22307eb05e17d9fa252d7ac5b4131ee51163
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2021-02-28 15:22:33 +0200

    Fix firedrake helper tests

--- tests/firedrake/test_helpers.py
+++ tests/firedrake/test_helpers.py
@@ -31,12 +31,20 @@ def test_from_jax_constant(test_input, expected):
     assert numpy.allclose(fenics_test_input.values(), expected.values())
 
 
+def _x0(mesh):
+    x = firedrake.SpatialCoordinate(mesh)
+    return x[0]
+
+
 @pytest.mark.parametrize(
     "test_input,expected_expr",
     [
-        (make_shaped_array(jax.numpy.ones(10)), "0.0"),
-        (jax.ad_util.Zero(get_aval(jax.numpy.asarray(0.0))), "0.0"),
-        (get_aval(jax.numpy.linspace(0.05, 0.95, num=10)), "x[0]"),
+        (make_shaped_array(jax.numpy.ones(10)), lambda mesh: firedrake.Constant(0.0)),
+        (
+            jax.ad_util.Zero(get_aval(jax.numpy.asarray(0.0))),
+            lambda mesh: firedrake.Constant(0.0),
+        ),
+        (get_aval(jax.numpy.linspace(0.05, 0.95, num=10)), _x0),
     ],
 )
 def test_from_jax_function(test_input, expected_expr):
@@ -44,7 +52,7 @@ def test_from_jax_function(test_input, expected_expr):
     V = firedrake.FunctionSpace(mesh, "DG", 0)
     template = firedrake.Function(V)
     fenics_test_input = from_jax(test_input, template)
-    expected = firedrake.interpolate(firedrake.Expression(expected_expr, degree=1), V)
+    expected = firedrake.interpolate(expected_expr(mesh), V)
     assert numpy.allclose(
         fenics_test_input.vector().get_local(), expected.vector().get_local()
     )
